d6e861052945343847a365224e803d99fe95bdfd
hamzseven <32614329+hamzseven@users.noreply.github.com>
2017-10-18 19:36:33 +0700

    Add files via upload

---
+++ Caramasak.java
@@ -0,0 +1,21 @@
+class masaknasi{
+
+public static void main(String[] args) {
+ tutorialmasaknasi();
+ }
+
+ public static void tutorialmasaknasi(){
+ System.out.println("1.  Ukur beras dengan cangkir takar, dan masukkan ke dalam panci penanak nasi Anda");
+ System.out.println("2.  Cuci beras jika perlu");
+ System.out.println("3.  Ukur Air");
+ System.out.println("4.  Rendam beras selama tiga puluh menit kalau suka");
+ System.out.println("5.  Tambahkan perasa (sesuai selera)");
+ System.out.println("6.  Masukkan butiran beras yang menempel di dinding panci masuk ke dalam air sehingga semua beras terendam sepenuhnya, jangan sampai ada yang tercecer");
+ System.out.println("7.  Periksa penanak nasi Anda untuk menyalakan pilihan atau pengaturan khusus.");
+ System.out.println("8.  Masak nasi dalam penanak nasi");
+ System.out.println("9.  Biarkan nasi selama 10-15 menit sebelum membuka tutup penanak nasi (pilihan)");
+ System.out.println("10. Aduk nasi agar tidak padat dan menggumpal, dan sajikan");
+ 
+ }
+
+}
